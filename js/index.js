12db13f020fadc001177c83a87f1f9d43107436a
Habib-0007 <adebayohabib7@gmail.com>
2024-02-12 14:38:54 +0100

    .

--- js/index.js
+++ js/index.js
@@ -1,7 +1,38 @@
-let canvas = document.getElementById("myCanvas");
-
 window.addEventListener("load", () => {
-	var ctx = canvas.getContext("2d")
-	
-	ctx.fillRect(10, 10, 200, 200)
+	var canvas = document.getElementById("myCanvas");
+	var ctx = canvas.getContext("2d");
+
+	var drawing = false;
+
+	var start = event => {
+		drawing = true;
+		draw(event);
+	};
+
+	var end = () => {
+		drawing = false;
+		ctx.beginPath();
+	};
+
+	var draw = event => {
+		if (!drawing) return;
+		ctx.lineWidth = 10;
+		ctx.lineCap = "round";
+		ctx.strokeStyle = "#000";
+
+		ctx.lineTo(
+			event.clientX - event.offsetLeft,
+			event.clientY - event.offsetTop
+		);
+		ctx.stroke();
+		ctx.beginPath();
+		ctx.moveTo(
+			event.clientX - event.offsetLeft,
+			event.clientY - event.offsetTop
+		);
+	};
+
+	canvas.addEventListener("mousedown", start);
+	canvas.addEventListener("mouseup", end);
+	canvas.addEventListener("mousemove", draw);
 });
